cc89815f2833be067c7fd8f695b0bc56c6499f02
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-12 10:47:43 +0300

    ISeriesRichRelationalModel implemented

---
+++ src/Pure.Diagram.RichRelationalModel.Abstractions/ISeriesRichRelationalModel.cs
@@ -0,0 +1,6 @@
+using Pure.Diagram.Model.Abstractions;
+using Pure.Diagram.RelationalModel.Abstractions;
+
+namespace Pure.Diagram.RichRelationalModel.Abstractions;
+
+public interface ISeriesRichRelationalModel : ISeries, ISeriesRelationalModel;
